08a027968dcfc7d68788805a24f3e0234f433fba
ophir-friedler <ophirfriedler@gmail.com>
2025-06-08 15:35:11 +0300

    npm install express mysql2 body-parser

---
+++ HairDresser/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Configure your MySQL connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'your_mysql_user',
+  password: 'your_mysql_password',
+  database: 'hairdresser'
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('Connected to MySQL');
+});
+
+// Endpoint to receive bookings
+app.post('/book', (req, res) => {
+  const { whatsapp, datetime } = req.body;
+  const sql = 'INSERT INTO bookings (whatsapp, datetime) VALUES (?, ?)';
+  db.query(sql, [whatsapp, datetime], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Database error');
+    }
+    // Optionally, notify yourself here (e.g., desktop notification)
+    res.sendStatus(200);
+  });
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
